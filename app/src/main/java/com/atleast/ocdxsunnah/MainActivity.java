229a26e3e7f7083854cedab1f6b4fc98ee6a0f97
abduh00 <abduhmajid123@gmail.com>
2023-11-29 13:28:56 +0700

    ocdproject

---
+++ app/src/main/java/com/atleast/ocdxsunnah/MainActivity.java
@@ -0,0 +1,17 @@
+package com.atleast.ocdxsunnah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "djgfjhdsj", Toast.LENGTH_SHORT).show();
+    }
+}
